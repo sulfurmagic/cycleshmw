f8a8da1fcd071a9984d528079aeb491320623331
sulfurmagic <johnaul81@gmail.com>
2024-02-22 16:49:18 +0300

    reformated code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,7 @@ import ru.netology.homework.cycleshmw.services.VacationMonthsService;
 public class Main {
 
     public static void main(String[] args) {
-       /* int income = 10_000;
-        int expenses = 101_000;
-        int threshold = 5_000;
-*/
+
         VacationMonthsService service = new VacationMonthsService();
         System.out.println(service.calcMonths(100_000, 60_000, 150_000));
 
